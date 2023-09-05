d5cce12120fe7d9bb61c5545e38529892041b5dd
Scorbunny <100584937+Scorbunny10@users.noreply.github.com>
2023-09-06 00:13:28 +0200

    Update code.js

--- code.js
+++ code.js
@@ -3,7 +3,7 @@
 // @icon https://static2.strzelce360.pl/data/wysiwig/wqtv1fum7knmhdb.jpg
 // @author Scorbunny
 // @namespace   https://github.com/Scorbunny10/Gigachader/tree/main
-// @version     1.2
+// @version     1.1
 // @description Zgigachaduj wszyskie zdjęcia na stronach
 // @include http://*/*
 // @include https://*/*
@@ -28,6 +28,31 @@ document.addEventListener('DOMContentLoaded', function() {
     img.style.height = originalHeight + 'px';
   });
 });
+(function() {
+    'use strict';
 
+    // Tworzenie przycisku aktualizacji
+    var updateButton = document.createElement('button');
+    updateButton.innerHTML = 'Aktualizuj Skrypt';
+    updateButton.style.marginTop = '10px';
+    updateButton.addEventListener('click', function() {
+        // Usuwanie skryptu
+        var scriptElement = document.querySelector('script[src$="code.js"]');
+        if (scriptElement) {
+            scriptElement.remove();
+        }
+
+        // Pobieranie i dodawanie nowej aktualizacji
+        var newScript = document.createElement('script');
+        newScript.src = 'https://raw.githubusercontent.com/Scorbunny10/Gigachader/main/code.js'; // Tutaj podaj link do nowej aktualizacji
+        document.head.appendChild(newScript);
+    });
+
+    // Dodawanie przycisku pod skryptem
+    var scriptElement = document.querySelector('script[src$="code.js"]');
+    if (scriptElement) {
+        scriptElement.parentNode.insertBefore(updateButton, scriptElement.nextSibling);
+    }
+})();
 
 // Rozszerzenie to działa na TamperMonkey
