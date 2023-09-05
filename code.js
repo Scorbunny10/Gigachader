ff117ff06357fd0a0738db374857a8feaca85050
Scorbunny <100584937+Scorbunny10@users.noreply.github.com>
2023-09-05 22:40:54 +0200

    Create code.js

---
+++ code.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @name        Gigachader
+// @namespace   http://tampermonkey.net/
+// @version     1.0
+// @description Zgigachaduj wszyskie zdjęcia na stronach
+// @include http://*/*
+// @include https://*/*
+// @grant       GM_addStyle
+// ==/UserScript==
+
+document.addEventListener('DOMContentLoaded', function() {
+  const images = document.querySelectorAll('img');
+
+  images.forEach(img => {
+    const originalWidth = img.offsetWidth;
+    const originalHeight = img.offsetHeight;
+
+    img.src = 'https://media.tenor.com/epNMHGvRyHcAAAAC/gigachad-chad.gif';
+    img.style.width = originalWidth + 'px';
+    img.style.height = originalHeight + 'px';
+  });
+});
+
+//Add it in Tampermonkey
